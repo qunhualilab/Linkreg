f5a2e724a34d32810e36e92239c69d688abcf0e2
Qiuhai Zeng <71804132+Jason-Qiuhai@users.noreply.github.com>
2023-12-10 17:47:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Linkreg',
-    version='0.1',
+    version='0.0.1',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     # Add more metadata as needed
