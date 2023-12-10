86f41be2002cabe9390c525f40a1e0ef0f96df58
Qiuhai Zeng <71804132+Jason-Qiuhai@users.noreply.github.com>
2023-12-10 16:15:56 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Linkreg',
+    version='0.1',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    # Add more metadata as needed
+)
