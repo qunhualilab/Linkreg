d7a16f37036492cfc4dd1fc192c6cd3e81366ea8
Qiuhai Zeng <71804132+Jason-Qiuhai@users.noreply.github.com>
2024-01-30 10:22:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Linkreg',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
