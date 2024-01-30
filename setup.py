deff49001c7ff86121da346a15565e415f202590
Qiuhai Zeng <71804132+Jason-Qiuhai@users.noreply.github.com>
2024-01-29 23:16:34 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ from setuptools import setup, find_packages
 setup(
     name='Linkreg',
     version='0.0.2',
-    packages=find_packages()
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'Linkreg=Linkreg.Linkreg:Linkreg',  # "script_name=package.module:function"
+        ],
+    },
     # Add more metadata as needed
 )
